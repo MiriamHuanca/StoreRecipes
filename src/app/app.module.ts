d5d009e9976be6a84f6fa8d7aa0f22578772a9da
miriam mamani <mairim.acnauh@gmail.com>
2020-07-30 11:42:33 -0300

    setting up routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {RecipesComponent} from './recipes/recipes.component';
+import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
+import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
+import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
 import {DropdownDirective} from './shared/dropdown.directive';
-import { GegerarPdfComponent } from './gegerar-pdf/gegerar-pdf.component';
-import { GenerarJspdfComponent } from './generar-jspdf/generar-jspdf.component';
+import {GegerarPdfComponent} from './gegerar-pdf/gegerar-pdf.component';
+import {GenerarJspdfComponent} from './generar-jspdf/generar-jspdf.component';
 import {PdfViewerModule} from 'ng2-pdf-viewer';
-import {ShoppingListService} from "./shopping-list/shopping-list.service";
-import {RecipeService} from "./recipes/recipe.service";
+
+import {ShoppingListService} from './shopping-list/shopping-list.service';
+import {RecipeService} from './recipes/recipe.service';
+
+import {AppRoutingModule} from './app-routing.module';
 
 
 @NgModule({
@@ -31,6 +33,7 @@ import {RecipeService} from "./recipes/recipe.service";
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
+    // component of pdf
     GegerarPdfComponent,
     GenerarJspdfComponent
   ],
@@ -39,9 +42,11 @@ import {RecipeService} from "./recipes/recipe.service";
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
+    // module of pdf
     PdfViewerModule,
   ],
   providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

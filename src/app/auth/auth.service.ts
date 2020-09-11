4b262fa28a38269bb6717e44e6b9ecd6a81823dc
miriam mamani <mairim.acnauh@gmail.com>
2020-09-11 00:51:47 -0300

    Creating & Storing the auth state User data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {catchError} from 'rxjs/operators';
-import {throwError} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
+import {Subject, throwError} from 'rxjs';
+import {User} from './user.model';
 
 export interface AuthResponseData {
   kind: string;
@@ -9,12 +10,14 @@ export interface AuthResponseData {
   email: string;
   refreshToken: string;
   expiresIn: string;
-  locald: string;
+  localId: string;
   registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
+  user = new Subject<User>();
+
   constructor(private http: HttpClient) {
   }
 
@@ -28,7 +31,10 @@ export class AuthService {
       }
     )
       .pipe(
-        catchError(this.handlieError)
+        catchError(this.handlieError),
+        tap(resData => {
+          this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+        })
       );
   }
 
@@ -42,8 +48,19 @@ export class AuthService {
       }
     )
       .pipe(
-        catchError(this.handlieError)
+        catchError(this.handlieError),
+        tap(resData => {
+          this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+        })
       );
+  }
+
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(
+      new Date().getTime() + expiresIn * 1000
+    );
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
   }
 
   private handlieError(errorRes: HttpErrorResponse) {

157d767fa0d62472061e88bb1c7047715a9ea771
miriam mamani <mairim.acnauh@gmail.com>
2020-09-27 21:41:02 -0300

    Getting atarted with Feature Module (Recipes Module)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,6 @@ import {AppRoutingModule} from './app-routing.module';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {RecipesComponent} from './recipes/recipes.component';
-import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
-import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
 import {DropdownDirective} from './shared/dropdown.directive';
@@ -21,30 +17,22 @@ import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {RecipeService} from './recipes/recipe.service';
 import {AuthInterceptorService} from './auth/auth-interceptor.service';
 
-import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
-import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
 import {AuthComponent} from './auth/auth.component';
 import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.component';
 import {AlertComponent} from './shared/alert/alert.component';
 import {PlaceholderDirective} from './shared/placeholder/placeholder.directive';
+import {RecipesModule} from './recipes/recipes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
     // component of pdf
     GegerarPdfComponent,
     GenerarJspdfComponent,
-    // recipes
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     // loading
     LoadingSpinnerComponent,
@@ -62,6 +50,7 @@ import {PlaceholderDirective} from './shared/placeholder/placeholder.directive';
     AppRoutingModule,
     // module of pdf
     PdfViewerModule,
+    RecipesModule,
   ],
   providers: [
     // the service is correctly here
